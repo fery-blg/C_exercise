e343b2f8032f1cb640c6427aac037761796f89ae
fery-blg <ferielblg5@gmail.com>
2025-05-02 10:09:10 +0100

    s2 dev ex 2

--- Devoir s2/Examen.c
+++ Devoir s2/Examen.c
@@ -1,35 +1,42 @@
 //Exercice 1 
 #include <stdio.h>
 // Saisir deux entiers A et B par adresse
-void Saisie(int *PA , int *PB) {
-    printf ("Donner la valeur de l'entier pointé par PA : ");
-    scanf ("%d",PA) ;
-    printf ("Donner la valeur de l'entier pointé par PB : ");
-    scanf ("%d",PB) ;
+void Saisie(int *A , int B) {
+    printf ("Donner la valeur de l'entier pointe par A : ");
+    scanf ("%d",A) ;
+    printf ("Donner la valeur de l'entier pointe par B : ");
+    scanf ("%d",B) ;
     }
 
 //2.function qui prend en parmetre un point vers un entier et incrementer sa val 
-    int increamntercval(int *A){
+    int Incval(int *A){
         *A=*A+1;
 
     }
 
 
 
+//3. function qui prend deucv val et invrement du adr
 
+  
+    
 
-//the main function 
+//the main function  
 int main (){
     
     
     int A,B;
+    int *PA,*PB;
+    PA=&A;
+    PB=&B;
     Saisie(&A,&B) ; 
     printf ("A = %d\n", A) ;
     printf ("B = %d\n", B) ;
-    increamntercval(&A);
-    increamntercval(&B);
+    Incval(&A);
+    Incval(&B);
     printf ("A = %d\n", A) ;
     printf ("B = %d\n", B) ;
+    
 
 
 }
--- Devoir s2/exemen2.c
+++ Devoir s2/exemen2.c
@@ -1,4 +1,89 @@
-//Exercise 2
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int NB_MOTS= 5 ;
+#define MAX_LONGUEUR_MOT 50
+// saise 
+char ** Saisie(char **tab) {
+    printf("Saisir %d mots (maximum %d caractères chacun):\n", NB_MOTS, MAX_LONGUEUR_MOT);
+    char **p ;
+     for (p = tab; p < tab+NB_MOTS; p++){
+        *p= (char *)malloc((MAX_LONGUEUR_MOT + 1) * sizeof(char)); 
+        scanf("%s", *p); 
+    }
+    return tab ;
+}
+// inserer mot ♥♥♣
+char** insererMot(char **tab) {
+    NB_MOTS=NB_MOTS+1 ; 
+    tab = (char **) realloc (tab, (NB_MOTS)*sizeof(char*)) ; 
+     
+   *(tab+(NB_MOTS-1)) = (char *)malloc((MAX_LONGUEUR_MOT + 1) * sizeof(char));
+   printf("\n Donner une chaine : ") ;
+   scanf("%s", *(tab+(NB_MOTS-1)));
+   return tab ;
+}
+// inverser mots
+void inverserMot(char *mot) {
+    char *p1 = mot;                       
+    char *p2 = mot + strlen(mot) - 1;   
+    char temp ; 
+    while (p1 < p2) {
+        temp = *p1;
+        *p1 = *p2;
+        *p2 = temp;
+        p1++;
+        p2--;
+    }
+}
+// inverser 
+void inverserTab(char **tab) {
+    char **p1 = tab;                      
+    char **p2 = tab + NB_MOTS - 1;        
+    char **temp ; 
+    while (p1 < p2) {
+        *temp = *p1;
+        *p1 = *p2;
+        *p2 = *temp;
+        p1++;
+        p2--;
+    }
+    char **p ;
+    for (p = tab; p < tab+NB_MOTS; p++) {
+        inverserMot(*p);
+    }
+    
+}
+//affiche
+void Afficher(char **tab) {
+    printf("Contenu du tableau de mots :\n");
+    char **p ;
+    for (p = tab; p < tab+NB_MOTS; p++) {
+        printf("%s\n", *p);
+    }
+}
+// libration
+void Liberer(char **tab) {
+    char **p ;
+    for (p = tab; p < tab+NB_MOTS; p++) {
+        free(*p);
+    }
+    free(tab) ;
+}
 
+// main func
+int main()
+{
+    
+    char **tab;  
+    tab = (char **)malloc(NB_MOTS * sizeof(char*)); 
+    tab = Saisie(tab);
+    printf("Tableau de mots avant l'inversion :\n");
+    Afficher(tab);
+    inverserTab(tab);
+    printf("Tableau de mots après l'inversion :\n");
+    Afficher(tab); 
+    Liberer(tab)  ;
+    Afficher(tab); 
+}
